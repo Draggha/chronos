8a523367cd8f5993a18892b70ef5b40cdd5e0833
Draggha <draggha@gmail.com>
2015-01-09 23:09:47 +0100

    Added inspiration folder with old code of mine in which I already took a stab at character creation processes. Well, at least the HTML and CSS will be usable

---
+++ inspiration/character sheets/resources/rules/WoDCC_rules_CtL.js
@@ -0,0 +1,11 @@
+/* @author: Johann Haaf
+ * 
+ */
+
+// CtL extends WoD
+CtL.prototype = new WoD;
+CtL.prototype.constructor = CtL;
+
+function CtL() {
+
+}
---
+++ inspiration/character sheets/resources/rules/WoDCC_rules_WoD.js
@@ -0,0 +1,13 @@
+/* @author: Johann Haaf
+ * 
+ */
+
+var AttrFirst = 5;
+var AttrSecond = 4;
+var AttrThird = 3;
+
+var WoD = {};
+
+WoD.ruleSetAttributes = function(e) {
+	alert('Test!');
+};
